f55fa14ff49b1a5024e7372160fb1d8e0442e808
chris laframboise <hire.code.seegreatness@gmail.com>
2018-10-26 22:30:40 -0600

    Update discord theme.user.js

--- discord theme.user.js
+++ discord theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         discord theme
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Creates and sets a custom theme for discord
 // @author       SeeGreatness
 // @match        https://*.discordapp.com/*
@@ -13,7 +13,10 @@
 // ==/UserScript==
 
 (function() {
+    
     'use strict';
+//fast code
+    document.getElementsByName("Discord")[0].outerHTML='<span name="Discord" id="orb" class="homeIcon-1FoKUJ" width="16" height="16"><div><br></div> <style> #orb{ background: #222; overflow: hidden; width: 10px; height: 10px;}.view { position: fixed; top: 0; left: 0; right: 1; bottom: 1; -webkit-perspective: 400; perspective: 400;}.plane { width: 120px; height: 120px; -webkit-transform-style: preserve-3d; transform-style: preserve-3d;}.plane.main { position: absolute; top: 0; left: 0; right: 0; bottom: 0; margin: auto; -webkit-transform: rotateX(60deg) rotateZ(-30deg); transform: rotateX(60deg) rotateZ(-30deg); -webkit-animation: rotate 20s infinite linear; animation: rotate 20s infinite linear;}.plane.main .circle { width: 120px; height: 120px; position: absolute; -webkit-transform-style: preserve-3d; transform-style: preserve-3d; border-radius: 100%; box-sizing: border-box; box-shadow: 0 0 60px crimson, inset 0 0 60px crimson;}.plane.main .circle::before, .plane.main .circle::after { content: ""; display: block; position: absolute; top: 0; left: 0; right: 0; bottom: 0; margin: auto; width: 10%; height: 10%; border-radius: 100%; background: crimson; box-sizing: border-box; box-shadow: 0 0 60px 2px crimson;}.plane.main .circle::before { -webkit-transform: translateZ(-90px); transform: translateZ(-90px);}.plane.main .circle::after { -webkit-transform: translateZ(90px); transform: translateZ(90px);}.plane.main .circle:nth-child(1) { -webkit-transform: rotateZ(72deg) rotateX(63.435deg); transform: rotateZ(72deg) rotateX(63.435deg);}.plane.main .circle:nth-child(2) { -webkit-transform: rotateZ(144deg) rotateX(63.435deg); transform: rotateZ(144deg) rotateX(63.435deg);}.plane.main .circle:nth-child(3) { -webkit-transform: rotateZ(216deg) rotateX(63.435deg); transform: rotateZ(216deg) rotateX(63.435deg);}.plane.main .circle:nth-child(4) { -webkit-transform: rotateZ(288deg) rotateX(63.435deg); transform: rotateZ(288deg) rotateX(63.435deg);}.plane.main .circle:nth-child(5) { -webkit-transform: rotateZ(360deg) rotateX(63.435deg); transform: rotateZ(360deg) rotateX(63.435deg);}@-webkit-keyframes rotate { 0% { -webkit-transform: rotateX(0) rotateY(0) rotateZ(0); transform: rotateX(0) rotateY(0) rotateZ(0); } 100% { -webkit-transform: rotateX(360deg) rotateY(360deg) rotateZ(360deg); transform: rotateX(360deg) rotateY(360deg) rotateZ(360deg); }}@keyframes rotate { 0% { -webkit-transform: rotateX(0) rotateY(0) rotateZ(0); transform: rotateX(0) rotateY(0) rotateZ(0); } 100% { -webkit-transform: rotateX(360deg) rotateY(360deg) rotateZ(360deg); transform: rotateX(360deg) rotateY(360deg) rotateZ(360deg); }} </style> <div class="view"> <div class="plane main"> <div class="circle"></div> <div class="circle"></div> <div class="circle"></div> <div class="circle"></div> <div class="circle"></div> <div class="circle"></div> </div></div></span>'
 
     // Your code here...
     //config code goes here
